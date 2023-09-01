7e5f07db2b7290b4e0f1ddcbcc64cc0a26aabb12
Antony Gakuru <antony123muriithi@gmail.com>
2023-09-01 20:34:55 +0300

    add player sprite

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ pygame.init()
 WINDOW_WIDTH = 1400
 WINDOW_HEIGHT= 800
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-title = pygame.display.set_caption("Explorer")
+title = pygame.display.set_caption("Njoro The Explorer")
 
+# background
 bg = pygame.image.load('assets/images/jungle.jpg')
 bg = pygame.transform.scale(bg, (WINDOW_WIDTH, WINDOW_HEIGHT))
-# bg_rect = bg.get_rect()
-# bg_x = (WINDOW_WIDTH - bg_rect.width ) // 2
-# bg_y = (WINDOW_HEIGHT - bg_rect.height ) // 2
+
+njoro = pygame.image.load("./assets/images/player/walking/kid2.png")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = njoro  # Use the loaded sprite image
+        self.rect = self.image.get_rect()
+        self.rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2) 
 
 running = True
 i = 0
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
 while running:
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -28,6 +40,10 @@ while running:
             i=0
             print("Right key pressed")
         i-=5
+    all_sprites.update()
+
+    all_sprites.draw(screen)
+
 
 
 
